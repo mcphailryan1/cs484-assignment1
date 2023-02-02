6d245c2b53cffb8a52a9e162aca0f6b8ce886060
Ryan McPhail <72577683+mcphailryan1@users.noreply.github.com>
2023-02-01 18:19:26 -0600

    adding spacing for questions

--- question_three.py
+++ question_three.py
@@ -12,28 +12,39 @@ from sklearn.neighbors import KNeighborsClassifier
 Fraud = pd.read_csv('Fraud.csv')
 Fraud.describe()
 
-#question3A
+
+def answerPrint2():
+    print("\nQuestion 3: The data FRAUD.csv contains the results of fraud investigations of 5,960 cases.  The binary variable FRAUD indicates the result of a fraud investigation: 1 = Fraud, 0 = Not Fraud.  The other quantitative variables contain information about the cases.")
+
+
 def threeA():
+    print("\nQuestion 3A: What percent of investigations are found to be frauds?  This is the empirical fraud rate.  Please round your answers to the fourth decimal place.\n")
     isFraud = Fraud.where(Fraud['FRAUD'] == 1)
     isFraudCount = isFraud.count()
     FraudPercentage = isFraud['CASE_ID'].count() / Fraud['CASE_ID'].count()
-    print(f'Empirical Fraud Percentage: {FraudPercentage}')
-    
-def threeC():
-    ## KNeighborClassifier
-    
-    for i in range(2,8):
-        Fraud_wIndex = Fraud.set_index('CASE_ID')
-        trainData = Fraud_wIndex[['TOTAL_SPEND', 'DOCTOR_VISITS', 'NUM_CLAIMS', 'NUM_MEMBERS', 'OPTOM_PRESC', 'MEMBER_DURATION']].dropna().reset_index()
+    print('Question 3A Answer: Empirical Fraud Percentage: ', FraudPercentage)
 
-        X = trainData[['TOTAL_SPEND', 'DOCTOR_VISITS', 'NUM_CLAIMS', 'NUM_MEMBERS', 'OPTOM_PRESC', 'MEMBER_DURATION']]
+
+def threeC():
+    # KNeighborClassifier
+    print("\nQuestion 3C Answer: ")
+
+    for i in range(2, 8):
+        Fraud_wIndex = Fraud.set_index('CASE_ID')
+        trainData = Fraud_wIndex[['TOTAL_SPEND', 'DOCTOR_VISITS', 'NUM_CLAIMS',
+                                  'NUM_MEMBERS', 'OPTOM_PRESC', 'MEMBER_DURATION']].dropna().reset_index()
+
+        X = trainData[['TOTAL_SPEND', 'DOCTOR_VISITS', 'NUM_CLAIMS',
+                       'NUM_MEMBERS', 'OPTOM_PRESC', 'MEMBER_DURATION']]
         y = trainData['CASE_ID']
 
-        kNC = KNeighborsClassifier(n_neighbors = i, metric = 'euclidean')
+        kNC = KNeighborsClassifier(n_neighbors=i, metric='euclidean')
         nbrs = kNC.fit(X, y)
 
         class_proba = nbrs.predict_proba(X)
-        print('Score with', i ,'neighbors: ', kNC.score(X, y))
-        
+        print('Score with', i, 'neighbors: ', kNC.score(X, y))
+
+
+answerPrint2()
 threeA()
 threeC()
